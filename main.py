3eecf4b06573792345e172d8b6e8a518de014bd6
Sana1008 <sanskrutidhanani78@gmail.com>
2026-01-09 14:20:10 -0800

    Add dirlist.txt and update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ sub_list = open(subdomains_path).read()
 subdoms = sub_list.splitlines()
 
 # target = input("Enter the target domain (e.g. example.com, example.org, etc.): ")
+print("Subdomain enumeration:")
 
 for sub in subdoms:
     sub_domains = f"http://{sub}.{sys.argv[1]}" 
@@ -21,4 +22,18 @@ for sub in subdoms:
         pass
     
     else:
-        print("Valid domain: ",sub_domains)   
+        print("Valid domain: ",sub_domains) 
+
+print("Directory enumeration:")
+
+Dir_path = os.path.join(script_dir, "dirlist.txt")
+dir_list = open(Dir_path).read() 
+directories = sub_list.splitlines()
+
+for dir in directories:
+    dir_enum = f"http://{sys.argv[1]}/{dir}.html" 
+    r = requests.get(dir_enum)
+    if r.status_code==404: 
+        pass
+    else:
+        print("Valid directory:" ,dir_enum)  
